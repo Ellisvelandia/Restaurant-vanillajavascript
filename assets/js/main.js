a0a554ab6c083ad9ab9e1bbaf98163bc5496b9d9
Ellisvelandia <eyis619l@gmail.com>
2023-05-02 13:47:14 -0500

    hero-section with swiper slider css

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,18 @@
 /*=============== REMOVE MENU MOBILE ===============*/
 
 /*=============== HOME SWIPER ===============*/
-
+var homeSswiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 /*=============== DEALS TAB ===============*/
 
 /*=============== SHOW SCROLL UP ===============*/

2680c7a8c2310c0afa01f6d75ea5be3afa26cf9c
Ellisvelandia <eyis619l@gmail.com>
2023-05-07 15:49:28 -0500

    footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,3 +42,16 @@ tabs.forEach((tab) => {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up')
+
+  if (this.scrollY >= 350) {
+    scrollUp.classList.add('show-scroll')
+  }else {
+    scrollUp.classList.remove('show-scroll')
+  } 
+    
+  
+}
+
+window.addEventListener("scroll", scrollUp)
